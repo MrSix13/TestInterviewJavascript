ca923c77da3b00a21a2596746f28ad994a935a2b
bastian <bastiangarcesga@gmail.com>
2023-01-17 17:47:36 -0300

    prueba 1

---
+++ index.ts
@@ -0,0 +1,28 @@
+let index:number = 1;//indice
+let side:number = 1003;//Numero de lados
+let size:number = side * side + 1;//Tamaño total
+let diagFirst:boolean = true;//switch para preguntar si hay o no primera diagonal
+let diagLeft:number = 1;//diagonal izquierda
+let diagRight:number = 1;//diagonal derecha
+let entry:number = 0; //entradas
+let gap:number = 2; //salto de numero
+
+for(let i:number = 1; i<size; i++){
+    if(i == index){
+        if(diagFirst){
+            diagLeft += i;
+            diagFirst = false;
+        }else{
+            diagRight += i;
+            diagFirst = true;
+        }
+        index += gap;
+        entry++
+    }
+    if(entry == 4){
+        entry = 0;
+        gap +=2
+    }
+
+}
+console.log( "total is = " + ( diagLeft + diagRight ));
